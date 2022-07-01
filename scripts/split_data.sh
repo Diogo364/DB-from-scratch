ccf454d1151d897440dbc1aed810a5923e4e7bf7
Diogo Nascimento <diogotnascimento94@gmail.com>
2022-07-01 15:23:03 -0400

    fix tmp path

--- scripts/split_data.sh
+++ scripts/split_data.sh
@@ -6,7 +6,7 @@ iconv -f utf-16le -t utf-8 $1 > /tmp/coded.csv
 
 echo "GENERATING REVENDA.CSV"
 cat /tmp/coded.csv | cut -f1-5,11 > /tmp/raw_revenda.csv
-awk -F '\t' ' {key =$5} !seen[key]++' /tmp/raw_revenda.csv > tmp/revenda.csv
+awk -F '\t' ' {key =$5} !seen[key]++' /tmp/raw_revenda.csv > /tmp/revenda.csv
 
 
 echo "GENERATING PRODUTO.CSV"
